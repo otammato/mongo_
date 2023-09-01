5ca2c4f5f0fd49a43e603985b2746fd4e957dce8
otammato <104728608+otammato@users.noreply.github.com>
2023-09-01 21:19:28 +0100

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,18 +1,18 @@
 // define default config, but allow overrides from ENV vars
 let config = {
-  APP_DB_HOST: "database-2.c9rglxpvlls0.us-east-1.rds.amazonaws.com",
-  APP_DB_USER: "admin",
-  APP_DB_PORT: "3306" ,
-  APP_DB_PASSWORD: "12345678",
+  APP_DB_HOST: "mongodb+srv://your-cluster-url", // replace with your MongoDB Atlas or local connection string
+  APP_DB_USER: "admin", // can be omitted if it's in the connection string
+  APP_DB_PASSWORD: "12345678", // can be omitted if it's in the connection string
   APP_DB_NAME: "COFFEE"
 }
 
 Object.keys(config).forEach(key => {
-  if(process.env[key] === undefined){
-    console.log(`[NOTICE] Value for key '${key}' not found in ENV, using default value.  See app/config/config.js`)
+  if (process.env[key] === undefined) {
+    console.log(`[NOTICE] Value for key '${key}' not found in ENV, using default value. See app/config/config.js`)
   } else {
     config[key] = process.env[key]
   }
 });
 
 module.exports = config;
+
