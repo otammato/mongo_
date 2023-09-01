cdc8a086a80fa22abd55952ab911c91be5305e04
otammato <104728608+otammato@users.noreply.github.com>
2023-09-01 21:37:14 +0100

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 // define default config, but allow overrides from ENV vars
 let config = {
-  APP_DB_HOST: "mongodb+srv://your-cluster-url", // replace with your MongoDB Atlas or local connection string
+  APP_DB_HOST: "localhost", // replace with your MongoDB Atlas or local connection string
   APP_DB_USER: "admin", // can be omitted if it's in the connection string
   APP_DB_PASSWORD: "12345678", // can be omitted if it's in the connection string
   APP_DB_NAME: "COFFEE"
