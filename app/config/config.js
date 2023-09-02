4f3da90c01835d832efcb76dfc67f7e5e7e724e2
otammato <104728608+otammato@users.noreply.github.com>
2023-09-02 23:01:03 +0100

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,18 +1,22 @@
-// define default config, but allow overrides from ENV vars
-let config = {
-  APP_DB_HOST: "mongodb://localhost:27017", // replace with your MongoDB Atlas or local connection string
-  // APP_DB_USER: "admin", // can be omitted if it's in the connection string
-  // APP_DB_PASSWORD: "12345678", // can be omitted if it's in the connection string
-  APP_DB_NAME: "coffee"
-}
+// // define default config, but allow overrides from ENV vars
+// let config = {
+//   APP_DB_HOST: "mongodb://localhost:27017", // replace with your MongoDB Atlas or local connection string
+//   // APP_DB_USER: "admin", // can be omitted if it's in the connection string
+//   // APP_DB_PASSWORD: "12345678", // can be omitted if it's in the connection string
+//   APP_DB_NAME: "coffee"
+// }
 
-Object.keys(config).forEach(key => {
-  if (process.env[key] === undefined) {
-    console.log(`[NOTICE] Value for key '${key}' not found in ENV, using default value. See app/config/config.js`)
-  } else {
-    config[key] = process.env[key]
-  }
-});
+// Object.keys(config).forEach(key => {
+//   if (process.env[key] === undefined) {
+//     console.log(`[NOTICE] Value for key '${key}' not found in ENV, using default value. See app/config/config.js`)
+//   } else {
+//     config[key] = process.env[key]
+//   }
+// });
 
-module.exports = config;
+// module.exports = config;
+
+module.exports = {
+    APP_DB_URI: "mongodb://localhost:27017/suppliersDB"
+};
 
