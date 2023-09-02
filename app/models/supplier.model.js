48441a0a83d4170fa92d03d9269ef9851fa5bf6c
otammato <104728608+otammato@users.noreply.github.com>
2023-09-02 22:19:26 +0100

    Update supplier.model.js

--- app/models/supplier.model.js
+++ app/models/supplier.model.js
@@ -1,106 +1,106 @@
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-const config = require("../config/config.js");
+const mongoose = require('mongoose');
+const dbConfig = "mongodb://localhost:27017/";
 
-let db;
+// Create mongoose connection
+mongoose.connect(dbConfig, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-MongoClient.connect(config.APP_DB_HOST, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, (err, client) => {
-  if (err) {
-    console.error('Failed to connect to the database');
-    throw err;
-  }
-  db = client.db(config.APP_DB_NAME);
-  console.log('Successfully connected to the database.');
+const SupplierSchema = new mongoose.Schema({
+    name: String,
+    address: String,
+    city: String,
+    state: String,
+    email: String,
+    phone: String
 });
 
-class Supplier {
-  constructor(supplier) {
-    this.id = supplier.id;
-    this.name = supplier.name;
-    this.address = supplier.address;
-    this.city = supplier.city;
-    this.state = supplier.state;
-    this.email = supplier.email;
-    this.phone = supplier.phone;
-  }
+const Supplier = mongoose.model('Supplier', SupplierSchema);
 
-  static create(newSupplier, result) {
-    db.collection('suppliers').insertOne(newSupplier, (err, res) => {
-      if (err) {
-        console.error("error: ", err);
-        return result(err, null);
-      }
-      console.log("created supplier: ", { id: res.insertedId, ...newSupplier });
-      return result(null, { id: res.insertedId, ...newSupplier });
-    });
-  }
+// ... remaining methods will be adapted to use Mongoose queries
 
-  static getAll(result) {
-    db.collection('suppliers').find({}).toArray((err, res) => {
-      if (err) {
-        console.error("error: ", err);
-        return result(err, null);
-      }
-      console.log("suppliers: ", res);
-      return result(null, res);
+Supplier.create = (newSupplier, result) => {
+    Supplier.create(newSupplier, (err, data) => {
+        if (err) {
+            console.log("error:", err);
+            result(err, null);
+            return;
+        }
+        console.log("created supplier:", data);
+        result(null, data);
     });
-  }
+};
 
-  static findById(supplierId, result) {
-    db.collection('suppliers').findOne({ _id: ObjectID(supplierId) }, (err, res) => {
-      if (err) {
-        console.error("error: ", err);
-        return result(err, null);
-      }
-      if (!res) {
-        return result({ kind: "not_found" }, null);
-      }
-      console.log("found supplier: ", res);
-      return result(null, res);
+Supplier.getAll = result => {
+    Supplier.find({}, (err, data) => {
+        if (err) {
+            console.log("error:", err);
+            result(err, null);
+            return;
+        }
+        console.log("suppliers:", data);
+        result(null, data);
     });
-  }
+};
 
-  static updateById(id, supplier, result) {
-    db.collection('suppliers').updateOne({ _id: ObjectID(id) }, { $set: supplier }, (err, res) => {
-      if (err) {
-        console.error("error: ", err);
-        return result(err, null);
-      }
-      if (res.matchedCount === 0) {
-        return result({ kind: "not_found" }, null);
-      }
-      console.log("updated supplier: ", { id: id, ...supplier });
-      return result(null, { id: id, ...supplier });
+Supplier.findById = (supplierId, result) => {
+    Supplier.findById(supplierId, (err, data) => {
+        if (err) {
+            console.log("error:", err);
+            result(err, null);
+            return;
+        }
+        if (!data) {
+            result({ kind: "not_found" }, null);
+            return;
+        }
+        console.log("found supplier:", data);
+        result(null, data);
     });
-  }
+};
 
-  static delete(id, result) {
-    db.collection('suppliers').deleteOne({ _id: ObjectID(id) }, (err, res) => {
-      if (err) {
-        console.error("error: ", err);
-        return result(err, null);
-      }
-      if (res.deletedCount === 0) {
-        return result({ kind: "not_found" }, null);
-      }
-      console.log("deleted supplier with id: ", id);
-      return result(null, res);
+Supplier.updateById = (id, supplierData, result) => {
+    Supplier.findByIdAndUpdate(id, supplierData, { new: true }, (err, data) => {
+        if (err) {
+            console.log("error:", err);
+            result(err, null);
+            return;
+        }
+        if (!data) {
+            result({ kind: "not_found" }, null);
+            return;
+        }
+        console.log("updated supplier:", data);
+        result(null, data);
     });
-  }
+};
 
-  static removeAll(result) {
-    db.collection('suppliers').deleteMany({}, (err, res) => {
-      if (err) {
-        console.error("error: ", err);
-        return result(err, null);
-      }
-      console.log(`deleted ${res.deletedCount} suppliers`);
-      return result(null, res);
+Supplier.delete = (id, result) => {
+    Supplier.findByIdAndDelete(id, (err, data) => {
+        if (err) {
+            console.log("error:", err);
+            result(err, null);
+            return;
+        }
+        if (!data) {
+            result({ kind: "not_found" }, null);
+            return;
+        }
+        console.log("deleted supplier with id:", id);
+        result(null, data);
     });
-  }
-}
+};
+
+Supplier.removeAll = result => {
+    Supplier.deleteMany({}, (err, data) => {
+        if (err) {
+            console.log("error:", err);
+            result(err, null);
+            return;
+        }
+        console.log(`deleted all suppliers`);
+        result(null, data);
+    });
+};
 
 module.exports = Supplier;
